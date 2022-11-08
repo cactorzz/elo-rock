254a2586c6587c0e9cacde1f25de5978d588bf02
Gabriel Moreira <gabrielmmm25@gmail.com>
2022-11-08 16:30:42 -0400

    change akali photo

--- src/componentes/pages/Home.js
+++ src/componentes/pages/Home.js
@@ -6,19 +6,21 @@ export default function Home(){
     return(
         <>
         <main id='mainHome' className='container-fluid flex-w'>
-            <div id="mainContainer">
-                <div id='textHome' >
+            <div id="mainContainer" className='container-fluid'>
+                <div id='textHome'className="" >
                     <h3 className="secundaryTxtHome">NÓS SOMOS A:</h3>
                     <h1 id='primaryTxtHome'>ROCK JOB</h1>
                     <h3 className="secundaryTxtHome">VENHA CONHECER NOSSOS SERVIÇOS</h3>
                 </div>
-                <div id='imgNextHome'>
-                    <img id='imgNext'src={nextImg}/>
+                <div id='nextImgContainer'>
+                    <img src={nextImg} id='nextImg'/>
                 </div>
-                <div id='imgsHome'>
+                <div id='imgsHome' className='container-fluid'>
                     <img className='img-fluid' id='akaliImg' src={akaliNeon}/>
                 </div>
+               
             </div>
+           
         </main>
         </>
     )
