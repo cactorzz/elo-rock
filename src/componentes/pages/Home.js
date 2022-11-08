f482b5f57b6f3b4455bd65193865eee17ab35d87
Gabriel Moreira <gabrielmmm25@gmail.com>
2022-11-08 18:00:21 -0400

    teste

--- src/componentes/pages/Home.js
+++ src/componentes/pages/Home.js
@@ -5,23 +5,21 @@ import estrelaNeon from '../imgs/estrelaNeon.svg'
 export default function Home(){
     return(
         <>
-        <main id='mainHome' className='container-fluid flex-w'>
-            <div id="mainContainer" className='container-fluid'>
-                <div id='textHome'className="" >
+        <main id='mainHome' className=''>
+            <section id="mainHomeContainer">
+                <div id="container-txt" >
                     <h3 className="secundaryTxtHome">NÓS SOMOS A:</h3>
                     <h1 id='primaryTxtHome'>ROCK JOB</h1>
                     <h3 className="secundaryTxtHome">VENHA CONHECER NOSSOS SERVIÇOS</h3>
                 </div>
-                <div id='nextImgContainer'>
-                    <img src={nextImg} id='nextImg'/>
-                </div>
-                <div id='imgsHome' className='container-fluid'>
+                {/* <img src={nextImg} id='nextImg'/> */}
+                <div id="container-img">
                     <img className='img-fluid' id='akaliImg' src={akaliNeon}/>
                 </div>
-               
-            </div>
-           
+            </section>
         </main>
+    
+    
         </>
     )
 }
