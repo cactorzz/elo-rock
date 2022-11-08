d95449b612e64a20365eb591c24d37d3d1aa4fea
Gabriel Moreira <gabrielmmm25@gmail.com>
2022-11-08 20:50:37 -0300

    edit home with grid

--- src/componentes/pages/Home.js
+++ src/componentes/pages/Home.js
@@ -7,14 +7,16 @@ export default function Home(){
         <>
         <main id='mainHome' className=''>
             <section id="mainHomeContainer">
-                <div id="container-txt" >
+                <div id="containerTxt" >
                     <h3 className="secundaryTxtHome">NÓS SOMOS A:</h3>
                     <h1 id='primaryTxtHome'>ROCK JOB</h1>
                     <h3 className="secundaryTxtHome">VENHA CONHECER NOSSOS SERVIÇOS</h3>
                 </div>
-                {/* <img src={nextImg} id='nextImg'/> */}
-                <div id="container-img">
+                <div id="containerImg">
                     <img className='img-fluid' id='akaliImg' src={akaliNeon}/>
+                </div>
+                <div id='containerNext'>
+                    <img src={nextImg}/>
                 </div>
             </section>
         </main>
