649d8e913afdc0fda1b64e6c6b79f67c55aed3d4
Gabriel Moreira <gabrielmmm25@gmail.com>
2022-11-18 17:54:26 -0400

    tirei layout de grid para display flex
    display grid tava bugando nas resolucoes, troquei pra display flex q consigo mexe melhor

--- src/componentes/pages/ServiceChoose/ServiceChoose.js
+++ src/componentes/pages/ServiceChoose/ServiceChoose.js
@@ -12,7 +12,8 @@ import mestreLol from '../../imgs/icones_elo_lol/mestreLol.png'
 export default function ServiceChoose(){
     return(
         <>
-        <section id='ServiceChooseMain'>   
+            <section id='ServiceChooseMain'> 
+        <div id="mainContent">
             <div className='div' id='headerService'>
                 <div className='container-fluid' id='headerChoose'>
                     <select id='selectService' className='form-select form-select-lg mb-3" aria-label=".form-select-lg example'>
@@ -23,6 +24,54 @@ export default function ServiceChoose(){
                     </select>
                 </div>
             </div>
+            
+            <div className='div' id='serviceContent'>
+                <div id='eloAtual'>
+                    <div className='txtServiceContent'>
+                        <h1>ELO ATUAL</h1>
+                    </div>
+                    <div className='imgServiceContent'>
+                        <img src={prataLol} className='imgEloService'></img>
+                        
+                    </div>
+                    <div className='divisionServiceContent'>
+                        <div className='containerDivImg'>
+                            <img src={ferroLol} className='imgEloDivision'></img>
+                            <img src={bronzeLol} className='imgEloDivision'></img>
+                            <img src={prataLol} className='imgEloDivision'></img>
+                            <img src={ouroLol} className='imgEloDivision'></img>
+                            <img src={platinaLol} className='imgEloDivision'></img>
+                            <img src={diamanteLol} className='imgEloDivision'></img>
+                        </div>
+                        {/* <img src={mestreLol} className='imgEloDivision'></img> */}
+                    </div>
+                </div>
+                <div id='eloDesejado'>
+                <div className='txtServiceContent'>
+                        <h1>ELO ATUAL</h1>
+                    </div>
+                    <div className='imgServiceContent'>
+                        <img src={diamanteLol} className='imgEloService'></img>
+                    </div>
+                    <div className='divisionServiceContent'>
+                        <div className='containerDivImg'>
+                            <img src={ferroLol} className='imgEloDivision'></img>
+                            <img src={bronzeLol} className='imgEloDivision'></img>
+                            <img src={prataLol} className='imgEloDivision'></img>
+                            <img src={ouroLol} className='imgEloDivision'></img>
+                            <img src={platinaLol} className='imgEloDivision'></img>
+                            <img src={diamanteLol} className='imgEloDivision'></img>
+                        </div>
+                        {/* <img src={mestreLol} className='imgEloDivision'></img> */}
+                    </div>
+                </div>
+            </div>
+            <div className='div' id='footerService'>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+            </div>
+        </div>  
+       
+        <div id="mainFinalizate">
             <div className='div item2' id='finalityService'>
                 <div id='txtFinality'>
                     <h2>SERVIÇOS ADICIONAIS</h2>
@@ -59,66 +108,18 @@ export default function ServiceChoose(){
                                 <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input"/><div id='textCheckbox'>Lorem ipsum dolor. </div>
                             </div>
                         </div>
-                        <div id='inputFilityContainer'>
-                            <div class="input-group-text" >
-                                <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input"/><div id='textCheckbox'>Lorem ipsum dolor. </div>
-                            </div>
-                        </div>
-                        
                        
-                        
+    
                     </div>              
                 </div>
                 <div id='finalizarFinality'>
-
+                    <p>teste</p>
                 </div>
+    
+            </div>
+        </div>
 
-            </div>
-            <div className='div' id='serviceContent'>
-                <div id='eloAtual'>
-                    <div className='txtServiceContent'>
-                        <h1>ELO ATUAL</h1>
-                    </div>
-                    <div className='imgServiceContent'>
-                        <img src={prataLol} className='imgEloService'></img>
-                    </div>
-                    <div className='divsionServiceContent'>
-                        <div className='containerDivImg'>
-                            <img src={ferroLol} className='imgEloDivision'></img>
-                            <img src={bronzeLol} className='imgEloDivision'></img>
-                            <img src={prataLol} className='imgEloDivision'></img>
-                            <img src={ouroLol} className='imgEloDivision'></img>
-                            <img src={platinaLol} className='imgEloDivision'></img>
-                            <img src={diamanteLol} className='imgEloDivision'></img>
-                        </div>
-                        {/* <img src={mestreLol} className='imgEloDivision'></img> */}
-                    </div>
-                </div>
-                <div id='eloDesejado'>
-                <div className='txtServiceContent'>
-                        <h1>ELO ATUAL</h1>
-                    </div>
-                    <div className='imgServiceContent'>
-                        <img src={diamanteLol} className='imgEloService'></img>
-                    </div>
-                    <div className='divsionServiceContent'>
-                        <div className='containerDivImg'>
-                            <img src={ferroLol} className='imgEloDivision'></img>
-                            <img src={bronzeLol} className='imgEloDivision'></img>
-                            <img src={prataLol} className='imgEloDivision'></img>
-                            <img src={ouroLol} className='imgEloDivision'></img>
-                            <img src={platinaLol} className='imgEloDivision'></img>
-                            <img src={diamanteLol} className='imgEloDivision'></img>
-                        </div>
-                        {/* <img src={mestreLol} className='imgEloDivision'></img> */}
-                    </div>
-                </div>
-            </div>
-            <div className='div' id='footerService'>
-                
-            </div>
-
-        </section>
+    </section>
         </>
     )
 }
