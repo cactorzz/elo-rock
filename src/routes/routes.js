ac64ea0eaff29b368be71e16881bc7eeff591d45
Gabriel Moreira <gabrielmmm25@gmail.com>
2022-11-26 11:38:29 -0400

    INSTALL REACTFIEBASEHOOKS

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '../componentes/pages/authenticate/Login.js'
+import { ServicePage } from '../componentes/pages/servicePage/ServicePage'
 
 export const AppRoutes = () =>{
     return(
         <BrowserRouter>
             <Routes>
-                {/* <Route path="ServicePage" element={<ServicePage/>}/> */}
                 <Route path="/" element={<Login/>}/>
-                
+                <Route path="ServicePage" element={<ServicePage/>}/> 
             </Routes>
         </BrowserRouter>
     )
